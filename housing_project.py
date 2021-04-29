e6b8442fbe82c1ac1508c6b504164ac3a73bb5bc
toddglad <twadeal@gmail.com>
2021-04-29 12:59:33 -0600

    second_commit

---
+++ housing_project.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import pandas as pd
+## df  = pd.read_csv(r'C:\Users\twade\Desktop\Housing Project\city_temperature.csv',engine = 'python')
+print(df)
