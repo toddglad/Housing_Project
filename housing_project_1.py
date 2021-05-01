20b13e56899caa95391d109d42c748cd8cfe406a
toddglad <twadeal@gmail.com>
2021-05-01 15:37:16 -0600

    Housing Project

---
+++ housing_project_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import pandas as pd
+## df  = pd.read_csv(r'C:\Users\twade\Desktop\Housing Project\city_temperature.csv',engine = 'python')
+print(df)
+
+
+### This is a test
