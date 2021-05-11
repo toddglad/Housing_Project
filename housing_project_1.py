8d5dc3410eb904e0d4b37beb04bac36c36b57a7f
toddglad <twadeal@gmail.com>
2021-05-10 18:14:06 -0600

    env

--- housing_project_1.py
+++ housing_project_1.py
@@ -6,9 +6,20 @@ This is a temporary script file.
 """
 
 
+
+
+
 import pandas as pd
+import googlemaps
+from datetime import datetime
+gmaps = googlemaps.Client(key='AIzaSyAIFjhJ68houXa379poru_HMRHpxrRzrY4')
+
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+##import pandas as pd
 ## df  = pd.read_csv(r'C:\Users\twade\Desktop\Housing Project\city_temperature.csv',engine = 'python')
-print(df)
+##print(df)
 
 
 ### This is a test
+## This is another test
