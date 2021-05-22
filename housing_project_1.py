39e956e6a1d82f6d0e241d99d1fd3ebae1312b8b
toddglad <twadeal@gmail.com>
2021-05-22 13:57:02 -0600

    May 22 Improvement

--- housing_project_1.py
+++ housing_project_1.py
@@ -1,72 +1,69 @@
-print("hello world")
-
+# print("hello world")
 
 
 import pandas as pd
-import googlemaps
+# import googlemaps
 from datetime import datetime
 from itertools import tee
 import json
-hello = 'Hello Todd'[::-1]
-
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
+# hello = 'Hello Todd'[::-1]
 
 
 
-print(hello)
-print("It worked")
+# print(hello)
+# print("It worked")
 
 df  = pd.read_csv(r'C:\Users\twade\Desktop\Housing Project\ChurchofJesusChristTemples.csv',engine = 'python')
-gmaps = googlemaps.Client(key='AIzaSyAIFjhJ68houXa379poru_HMRHpxrRzrY4')
+# gmaps = googlemaps.Client(key='AIzaSyAIFjhJ68houXa379poru_HMRHpxrRzrY4')
 
-print(df)
+# print(df)
 
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
+# def pairwise(iterable):
+#     a, b = tee(iterable)
+#     next(b, None)
+#     return zip(a, b)
 
-#empty list - will be used to store calculated distances
+# #empty list - will be used to store calculated distances
 list = []
+list1 = []
 
-# Loop through each row in the data frame using pairwise
-for (i1, row1), (i2, row2) in pairwise(df.iterrows()):
-      #Assign latitude and longitude as origin/departure points
-      LatOrigin = row1['Latitude'] 
-      LongOrigin = row1['Longitude']
-      origins = (LatOrigin,LongOrigin)
+# # Loop through each row in the data frame using pairwise
+# for (i1, row1), (i2, row2) in pairwise(df.iterrows()):
+#       #Assign latitude and longitude as origin/departure points
+#       LatOrigin = row1['Latitude'] 
+#       LongOrigin = row1['Longitude']
+#       origins = (LatOrigin,LongOrigin)
 
-      #Assign latitude and longitude from the next row as the destination point
-      LatDest = row2['Latitude']   # Save value as lat
-      LongDest = row2['Longitude'] # Save value as lat
-      destination = (LatDest,LongDest)
+#       #Assign latitude and longitude from the next row as the destination point
+#       LatDest = row2['Latitude']   # Save value as lat
+#       LongDest = row2['Longitude'] # Save value as lat
+#       destination = (LatDest,LongDest)
 
-      #pass origin and destination variables to distance_matrix function# output in meters
-      result = gmaps.distance_matrix(origins, destination, mode='driving', units = 'imperial')["rows"][0]["elements"][0]
-      #append result to list
-      list.append(result["distance"]["text"])
-      print(result["distance"]["text"])
-
-df['Distance']=list
-print("here")
-# 
-# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-# destination = (40.6943, -73.9249)
-# actual_duration = []
-
-# destinations = df[['Latitude', 'Longitude']]
+#       #pass origin and destination variables to distance_matrix function# output in meters
+#       result = gmaps.distance_matrix(origins, destination, mode='driving', units = 'imperial')["rows"][0]["elements"][0]
+#       #append result to list
+#       list.append(result["distance"]["text"])
+#       list1.append(result["duration"]["text"])
+#       print(result["distance"]["text"])
 
 
-# test = destinations.head()
-# for destination in destinations:
-#     result = gmaps.distance_matrix(origin, destination, mode='driving')["rows"][0]["elements"][0]["duration"]["value"]  
-#     result = result/3600
-#     actual_duration.append(result)
+# a = 0
+# list.insert(a,0)
+# list1.insert(a,0)
+# df["Distance"] = list
+# df["Duration"] = list1
+# print(df)
+####### This stuff all works.  Just working on the next portion.  
+
+df1 = pd.read_csv(r'C:\Users\twade\Desktop\Housing Project\Housing_Project\uscities.csv',engine = 'python')
+print(df1.head())
+
+
+for (i1,row1) in df1.iterrows(): ## There is problem here with only having i1, row1
+     LatOrigin = row1['lat'] 
+     LongOrigin = row1['lng']
+     origins = (LatOrigin,LongOrigin)
+     print(origins)
 
 
 
-### This is a test
-## This is another test
